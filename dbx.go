3f79b19e074c9579104ff9ccf9e7acbacf9c22e3
Ryan <ryanker1024@gmail.com>
2019-05-13 12:57:02 +0800

    init

---
+++ dbx.go
@@ -0,0 +1,530 @@
+package dbx
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type DB struct {
+	*sqlx.DB
+}
+
+type Query struct {
+	*DB
+	table    string
+	fields   []string
+	selector []Selector
+	orderBy  []string
+	skip     int64
+	limit    int64
+}
+
+type S []Selector
+
+type Selector struct {
+	Field  string
+	Symbol string
+	Value  interface{}
+}
+
+type D []DocElem
+
+type DocElem struct {
+	Field string
+	Value interface{}
+}
+
+func Open(driverName, dataSourceName string) (*DB, error) {
+	db, err := sqlx.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	LogInit()
+	return &DB{DB: db}, err
+}
+
+func (db *DB) Table(name string) *Query {
+	return &Query{DB: db, table: name}
+}
+
+func (q *Query) Fields(fields []string) *Query {
+	q.fields = fields
+	return q
+}
+
+func (q *Query) Find(selector S) *Query {
+	q.selector = selector
+	return q
+}
+
+func (q *Query) Sort(orderBy []string) *Query {
+	q.orderBy = orderBy
+	return q
+}
+
+func (q *Query) Limit(limit int64) *Query {
+	q.limit = limit
+	return q
+}
+
+func (q *Query) Skip(skip int64) *Query {
+	q.skip = skip
+	return q
+}
+
+func (q *Query) InsertS(st interface{}) (id int64, err error) {
+	id, err = q.Insert(S2D(st))
+	return
+}
+
+func (q *Query) Insert(data D) (id int64, err error) {
+	kStr, vStr, args := GetSqlInsert(data)
+	s := "INSERT INTO `" + q.table + "`(" + kStr + ") VALUES (" + vStr + ")"
+	LogWrite(s, args...)
+
+	var stmt *sql.Stmt
+	stmt, err = q.Prepare(s)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer stmt.Close()
+
+	var res sql.Result
+	res, err = stmt.Exec(args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+
+	id, err = res.LastInsertId()
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) InsertIgnore(data D) (id int64, err error) {
+	kStr, vStr, args := GetSqlInsert(data)
+	s := "INSERT IGNORE INTO `" + q.table + "`(" + kStr + ") VALUES (" + vStr + ")"
+	LogWrite(s, args...)
+
+	res, err := q.Exec(s, args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+
+	id, err = res.LastInsertId()
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) Replace(data D) (id int64, err error) {
+	kStr, vStr, args := GetSqlInsert(data)
+	s := "REPLACE INTO `" + q.table + "`(" + kStr + ") VALUES (" + vStr + ")"
+	LogWrite(s, args...)
+
+	res, err := q.Exec(s, args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+
+	id, err = res.LastInsertId()
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) Update(data D, where S) (n int64, err error) {
+	setStr, args := GetSqlUpdate(data)
+	whereStr, args2 := GetSqlWhere(where)
+	args = append(args, args2...)
+
+	s := "UPDATE `" + q.table + "` SET " + setStr + whereStr
+	LogWrite(s, args...)
+
+	var stmt *sql.Stmt
+	stmt, err = q.Prepare(s)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer stmt.Close()
+
+	var res sql.Result
+	res, err = stmt.Exec(args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+
+	n, err = res.RowsAffected()
+	return
+}
+
+func (q *Query) Delete(where S) (n int64, err error) {
+	whereStr, args := GetSqlWhere(where)
+	s := "DELETE FROM `" + q.table + "`" + whereStr
+	LogWrite(s, args...)
+
+	var stmt *sql.Stmt
+	stmt, err = q.Prepare(s)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer stmt.Close()
+
+	var res sql.Result
+	res, err = stmt.Exec(args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+
+	n, err = res.RowsAffected()
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) Count(where S) (n int64, err error) {
+	whereStr, args := GetSqlWhere(where)
+	s := "SELECT COUNT(*) FROM `" + q.table + "`" + whereStr
+	LogWrite(s, args...)
+
+	var stmt *sql.Stmt
+	stmt, err = q.Prepare(s)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(args...).Scan(&n)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) One(dest interface{}) (err error) {
+	fields := GetSqlFields(q.fields)
+	whereStr, args := GetSqlWhere(q.selector)
+	s := "SELECT " + fields + " FROM `" + q.table + "`" + whereStr + " LIMIT 1"
+	LogWrite(s, args...)
+
+	udb := q.Unsafe()
+	err = udb.Get(dest, s, args...)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			ErrorLogWrite(err, s, args...)
+		}
+	}
+	return
+}
+
+func (q *Query) All(dest interface{}) (err error) {
+	fields := GetSqlFields(q.fields)
+	whereStr, args := GetSqlWhere(q.selector)
+	orderStr := GetSqlOrderBy(q.orderBy)
+	limitStr := GetSqlLimit(q.skip, q.limit)
+	s := "SELECT " + fields + " FROM `" + q.table + "`" + whereStr + orderStr + limitStr
+	LogWrite(s, args...)
+
+	udb := q.Unsafe()
+	err = udb.Select(dest, s, args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	return
+}
+
+func (q *Query) OneMap() (row map[string]interface{}, columns []string, err error) {
+	whereStr, args := GetSqlWhere(q.selector)
+	fields := GetSqlFields(q.fields)
+	s := "SELECT " + fields + " FROM `" + q.table + "`" + whereStr
+	LogWrite(s, args...)
+
+	var rows *sql.Rows
+	rows, err = q.Query(s, args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		m := map[string]interface{}{}
+		columns, err = MapScan(rows, m)
+		if err != nil {
+			ErrorLogWrite(err, s, args...)
+			return
+		}
+		row = m
+	}
+	return
+}
+
+func (q *Query) AllMap() (list []map[string]interface{}, columns []string, err error) {
+	fields := GetSqlFields(q.fields)
+	whereStr, args := GetSqlWhere(q.selector)
+	orderStr := GetSqlOrderBy(q.orderBy)
+	limitStr := GetSqlLimit(q.skip, q.limit)
+	s := "SELECT " + fields + " FROM `" + q.table + "`" + whereStr + orderStr + limitStr
+	LogWrite(s, args...)
+
+	var rows *sql.Rows
+	rows, err = q.Query(s, args...)
+	if err != nil {
+		ErrorLogWrite(err, s, args...)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		m := map[string]interface{}{}
+		columns, err = MapScan(rows, m)
+		if err != nil {
+			ErrorLogWrite(err, s, args...)
+			return
+		}
+		list = append(list, m)
+	}
+	return
+}
+
+// 绑定到 Map
+func MapScan(r *sql.Rows, dest map[string]interface{}) (columns []string, err error) {
+	columns, err = r.Columns()
+	if err != nil {
+		return
+	}
+
+	values := make([]interface{}, len(columns))
+	for i := range values {
+		values[i] = new(interface{})
+	}
+
+	err = r.Scan(values...)
+	if err != nil {
+		return
+	}
+
+	for i, column := range columns {
+		dest[column] = *(values[i].(*interface{}))
+	}
+
+	err = r.Err()
+	return
+}
+
+func GetSqlFields(fields []string) (s string) {
+	if len(fields) > 0 {
+		for _, field := range fields {
+			s += "`" + field + "`,"
+		}
+		s = strings.TrimRight(s, ",")
+	} else {
+		s = "*"
+	}
+	return
+}
+
+func GetSqlOrderBy(arr []string) (s string) {
+	if len(arr) > 0 {
+		s = " ORDER BY "
+		for _, v := range arr {
+			v = strings.Replace(v, " ", "", -1)
+			symbol := v[0:1]
+			if symbol == "-" {
+				s += "`" + v[1:] + "` DESC,"
+			} else {
+				s += "`" + v + "` ASC,"
+			}
+		}
+		s = strings.TrimRight(s, ",")
+	}
+	return
+}
+
+func GetSqlLimit(skip, limit int64) (s string) {
+	if limit > 0 {
+		s = " LIMIT " + strconv.FormatInt(skip, 10) + "," + strconv.FormatInt(limit, 10)
+	}
+	return
+}
+
+func GetSqlInsert(data D) (kStr, vStr string, args []interface{}) {
+	for _, v := range data {
+		kStr += "`" + v.Field + "`, "
+		vStr += "?, "
+		args = append(args, v.Value)
+	}
+	kStr = strings.TrimSuffix(kStr, ", ")
+	vStr = strings.TrimSuffix(vStr, ", ")
+	return
+}
+
+func GetSqlUpdate(data D) (setStr string, args []interface{}) {
+	for _, v := range data {
+		symbol := v.Field[0:1]
+		if symbol == "+" || symbol == "-" {
+			field := v.Field[1:]
+			setStr += "`" + field + "`=`" + field + "`" + symbol + "?, "
+		} else {
+			setStr += "`" + v.Field + "`=?, "
+		}
+		args = append(args, v.Value)
+	}
+	setStr = strings.TrimSuffix(setStr, ", ")
+	return
+}
+
+func GetSqlWhere(selector S) (whereStr string, args []interface{}) {
+	if len(selector) == 0 {
+		return
+	}
+	whereStr = " WHERE "
+	for _, v := range selector {
+		if v.Symbol == "IN" {
+			s2 := ""
+			switch t := v.Value.(type) {
+			case []int:
+				arr := v.Value.([]int)
+				for _, v2 := range arr {
+					s2 += "?,"
+					args = append(args, v2)
+				}
+			case []int64:
+				arr := v.Value.([]int64)
+				for _, v2 := range arr {
+					s2 += "?,"
+					args = append(args, v2)
+				}
+			case []string:
+				arr := v.Value.([]string)
+				for _, v2 := range arr {
+					s2 += "?,"
+					args = append(args, v2)
+				}
+			default:
+				fmt.Println("Unsupported types:", t)
+			}
+			if s2 != "" {
+				s2 = strings.Trim(s2, ",")
+				whereStr += "`" + v.Field + "` IN (" + s2 + ") AND "
+			}
+		} else {
+			whereStr += "`" + v.Field + "` " + v.Symbol + " ? AND "
+			args = append(args, v.Value)
+		}
+	}
+	whereStr = strings.TrimSuffix(whereStr, " AND ")
+	return
+}
+
+func S2D(t interface{}) (d []DocElem) {
+	st := reflect.TypeOf(t)
+	sv := reflect.ValueOf(t)
+	for i := 0; i < st.NumField(); i++ {
+		f := st.Field(i)
+		if field := f.Tag.Get("db"); field != "" {
+			if strings.Contains(field, ",") {
+				arr := strings.Split(field, ",")
+				if arr[1] != "auto_increment" {
+					d = append(d, DocElem{arr[0], sv.Field(i).Interface()})
+				}
+			} else {
+				fmt.Println(field)
+				d = append(d, DocElem{field, sv.Field(i).Interface()})
+			}
+		}
+	}
+	return
+}
+
+var LogFile string
+var ErrorLogFile string
+
+var LogIoWriter io.Writer = os.Stdout
+
+func LogInit() {
+	if LogFile != "" {
+		var err error
+		LogIoWriter, err = os.OpenFile(LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func LogWrite(s string, args ...interface{}) {
+	if LogFile == "" {
+		return
+	}
+	_, err := fmt.Fprintf(LogIoWriter, "%v | %s\n",
+		time.Now().Format("2006-01-02 15:04:05"),
+		fmt.Sprintf(strings.Replace(s, "?", "'%v'", -1), ReplaceSlash(args...)...),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func ErrorLogWrite(e error, s string, args ...interface{}) {
+	if ErrorLogFile == "" {
+		return
+	}
+
+	f, err := os.OpenFile(ErrorLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	str := fmt.Sprintf("%v | ERROR: %v | SQL: %s\n",
+		time.Now().Format("2006-01-02 15:04:05"),
+		e.Error(),
+		fmt.Sprintf(strings.Replace(s, "?", "'%v'", -1), ReplaceSlash(args...)...),
+	)
+	if _, err := f.Write([]byte(str)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := f.Close(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func ReplaceSlash(args ...interface{}) []interface{} {
+	for k := range args {
+		if s, ok := args[k].(string); ok {
+			s = strings.Replace(s, "'", "\\'", -1)
+			s = strings.Replace(s, "\\", "\\\\", -1)
+			args[k] = s
+		}
+	}
+	return args
+}
---
+++ example/simple.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	".."
+)
+
+type User struct {
+	Uid        int64     `db:"uid,auto_increment"`
+	Gid        int64     `db:"gid"`
+	Name       string    `db:"name"`
+	CreateDate time.Time `db:"createDate"`
+}
+
+var err error
+var db *dbx.DB
+
+func main() {
+	os.Remove("./test.db")
+
+	// 开启日志
+	dbx.LogFile = "./db.log"
+	dbx.ErrorLogFile = "./db.error.log"
+
+	// 打开数据库
+	db, err = dbx.Open("sqlite3", "./test.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// 参数设置
+	db.SetMaxIdleConns(50)
+	db.SetMaxOpenConns(2000)
+	db.SetConnMaxLifetime(time.Second * 5)
+
+	// 创建表
+	_, err = db.Exec(`DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+  uid        INTEGER PRIMARY KEY AUTOINCREMENT,
+  gid        INTEGER NOT NULL DEFAULT '0',
+  name       TEXT             DEFAULT '',
+  name2      TEXT             DEFAULT '',
+  name3      TEXT             DEFAULT '',
+  createDate DATETIME         DEFAULT CURRENT_TIMESTAMP
+);`)
+	if err != nil {
+		panic(err)
+	}
+
+	// 插入
+	for i := 1; i <= 5; i++ {
+		uid, err := db.Table("user").Insert(dbx.D{
+			{"gid", 1},
+			{"name", "admin" + strconv.Itoa(i)},
+			{"createDate", time.Now().Format("2006-01-02 15:04:05")},
+		})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Insert:", uid)
+	}
+
+	for i := 1; i <= 5; i++ {
+		st := User{}
+		st.Gid = 3
+		st.Name = "twoTest" + strconv.Itoa(i)
+		st.CreateDate = time.Now()
+		uid, err := db.Table("user").InsertS(st)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Insert:", uid)
+	}
+
+	// 更新
+	n, err := db.Table("user").Update(dbx.D{
+		{"gid", 2},
+		{"name", "test"},
+	}, dbx.S{
+		{"uid", "=", 1},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("Update:", n)
+
+	// 统计数量
+	n, err = db.Table("user").Count(dbx.S{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("Count:", n)
+
+	// 读取一条 到 结构体
+	row := User{}
+	err = db.Table("user").Find(dbx.S{
+		{"uid", "=", 1},
+	}).One(&row)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Printf("Read: %+v\n", row)
+
+	// 读取多条 到 结构体
+	var list []User
+	err = db.Table("user").Find(dbx.S{}).All(&list)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("List:", list)
+	listByte, _ := json.Marshal(list)
+	fmt.Println("Json:", string(listByte))
+
+	// 读取一条 到 Map
+	rowMap, columns, err := db.Table("user").Find(dbx.S{
+		{"uid", "=", 1},
+	}).OneMap()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("row Map:", rowMap)
+	rowMapByte, _ := json.Marshal(rowMap)
+	fmt.Println("Json Map:", string(rowMapByte))
+	fmt.Println("columns:", columns)
+
+	// 读取多条 到 Map
+	listMap, columns, err := db.Table("user").Find(dbx.S{
+		{"uid", "<", 5},
+	}).AllMap()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("List Map:", listMap)
+	listMapByte, _ := json.Marshal(listMap)
+	fmt.Println("Json Map:", string(listMapByte))
+	fmt.Println("columns:", columns)
+
+	// 删除
+	n, err = db.Table("user").Delete(dbx.S{
+		{"uid", "=", 4},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	fmt.Println("Delete:", n)
+}
